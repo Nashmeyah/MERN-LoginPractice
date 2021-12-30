41c9b8cff55b5974789808dbc871536ab5d4bb03
Nashmeyah <nashalrekabi@gmail.com>
2021-12-29 20:44:02 -0700

    created server file and routes folder

---
+++ loginfront/loginbackend/routes/routes.js
@@ -0,0 +1,7 @@
+const { request, response } = require('express')
+const express = require('express')
+const router = express.Router()
+
+// router.post('/login', (request, response)) => {
+//   response.send('send')
+// }
