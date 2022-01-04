ecb3c86c781c5eb7606a93acd498167678e5901e
Nashmeyah <nashalrekabi@gmail.com>
2022-01-04 16:51:05 -0700

    react state setup

---
+++ loginfront/src/App.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state ={
+            firstName:"",
+            lastName:"",
+            email:"",
+            companyName:"",
+            password:""
+        }
+    }
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default App;
