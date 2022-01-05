ba6f2095807c0ffbd29ee39df79375fb5713386f
Nashmeyah <nashalrekabi@gmail.com>
2022-01-04 17:33:03 -0700

    bcrypt added and salted password

--- loginfront/loginbackend/routes/routes.js
+++ loginfront/loginbackend/routes/routes.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const signUpTemplateCopy = require('../models/SignUpModels')
+const bcrypt = require('bcrypt')
 
-router.post('/signup', (request, response) => {
+router.post('/signup', async (request, response) => {
+
+  const saltPassword = await bcrypt.genSalt(10)
+  const securePassword = await bcrypt.hash(request.body.password, saltPassword)
+
   const signedUpUser = new signUpTemplateCopy({
     firstName: request.body.firstName,
     lastName: request.body.lastName,
     email: request.body.email,
     companyName: request.body.companyName,
-    password: request.body.password
+    password: securePassword
 
   })
 
