1edc27f09da11b430f7add066b27c010c0c8da95
Nashmeyah <nashalrekabi@gmail.com>
2022-01-10 18:47:30 -0700

    form submits successfuly with updated hooks

--- loginfront/src/App.jsx
+++ loginfront/src/App.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import axios from "axios"
 
@@ -16,44 +16,49 @@ function App(){
         e.preventDefault()
 
         const registered ={
-            firstName:this.state.firstName,
-            lastName:this.state.lastName,
-            email:this.state.email,
-            companyName:this.state.companyName,
-            password:this.state.password
+            firstName:firstName,
+            lastName:lastName,
+            email:email,
+            companyName:companyName,
+            password:password
         }
 
         axios.post("http://localhost:4000/app/signup", registered)
             .then(response => console.log(response.data))
 
-        this.setState({
-            firstName:"",
-            lastName:"",
-            email:"",
-            companyName:"",
-            password:""
-        })
+
+            setFirstName("")
+            setLastName("")
+            setEmail("")
+            setCompanyName("")
+            setPassword("")
+
+        // this.setState({
+        //     firstName:"",
+        //     lastName:"",
+        //     email:"",
+        //     companyName:"",
+        //     password:""
+        // })
     }
 
-    // render() {
-        return(
-            <div>
-                <div className="container">
-                    <div className="form-div">
-                        <form onSubmit={registerUser}>
-                            <input type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} value={this.state.firstName} className="form-control form-group" />
-                            <input type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} value={this.state.lastName} className="form-control form-group"/>
-                            <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={this.state.email} className="form-control form-group"/>
-                            <input type="text" placeholder="Company Name" onChange={(e) => setCompanyName(e.target.value)} value={this.state.companyName} className="form-control form-group"/>
-                            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={this.state.password} className="form-control form-group"/>
+    return(
+        <div>
+            <div className="container">
+                <div className="form-div">
+                    <form onSubmit={registerUser}>
+                        <input type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} value={firstName} className="form-control form-group" />
+                        <input type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} value={lastName} className="form-control form-group"/>
+                        <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} className="form-control form-group"/>
+                        <input type="text" placeholder="Company Name" onChange={(e) => setCompanyName(e.target.value)} value={companyName} className="form-control form-group"/>
+                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} className="form-control form-group"/>
 
-                            <input type="submit" className="btn btn-danger btn-block" value="Submit"/>
-                        </form>
-                    </div>
+                        <input type="submit" className="btn btn-danger btn-block" value="Submit"/>
+                    </form>
                 </div>
             </div>
-        );
-    // }
+        </div>
+    );
 }
 
 export default App;
